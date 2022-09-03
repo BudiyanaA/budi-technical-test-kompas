d880948de661803b8877dcd6b5b8f9295d73a557
budiyanaa <asepbudiyana12@gmail.com>
2022-09-03 07:41:16 +0700

    create home page

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-1/5 rounded overflow-hidden shadow-lg">
+    <div class="px-4 py-2">
+      <div class="font-semibold text-base mb-2 text-left">18 Februari</div>
+        <table class="table-auto text-sm">
+          <tbody>
+            <tr v-for="index in 6" :key="index">
+              <td class="text-center border-b px-2 py-1">03.15</td>
+              <td class="text-left border-b px-2 py-1">Ayam Tangkap</td>
+              <td class="text-right border-b px-2 py-1">Rp 77.249</td>
+            </tr>
+            <tr>
+              <td class="font-bold border-t-4 px-2 py-1 text-right" colspan="2">Total</td>
+              <td class="font-bold text-right border-t-4 px-2 py-1">Rp 77.249</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  </div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="text-2xl font-semibold">Diari Jajan Februari 2021</h1>
+  <h2 class="text-lg">Pengeluaran Bulan Ini Rp 5.605.475</h2>
+  <button class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded mt-4">
+    Tambah Item
+  </button>
+  <div class="flex flex-wrap my-8 gap-4 justify-center">
+    <Card v-for="index in 8" :key="index" />
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Card,
+  }
+}
+</script>
